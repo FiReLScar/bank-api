4ab73070f3e72f1a3dfdf8a55a66ae21dc5f50af
FiRe <levih@n11.dev>
2022-12-14 03:54:01 -0600

    Login: done and added bal check and save 2fa questions
    Signed-off-by: FiRe <levih@n11.dev>

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 let axios = require('axios')
+let fs = require('fs')
 
 let Session = {
-    ID: '', ScriptManagerContent: '', TabLogin: '', EventTarget_: '', EventArgument_: '', ViewState: '', ViewStateGenerator: '', EventValidation: '', KeyID: '', SecureCookieKey: ''
+    ID: '',
+    ScriptManagerContent: '',
+    TabLogin: '',
+    EventTarget_: '',
+    EventArgument_: '',
+    ViewState: '',
+    ViewStateGenerator: '',
+    EventValidation: '',
+    KeyID: '',
+    SecureCookieKey: '',
+    Question: '',
+    Answer: '',
+    Username: ''
+}
+
+let Data = {
+    Accounts: []
 }
 
 let CreateSession = async () => {
@@ -84,9 +101,11 @@ let Username = async username => {
     Session.EventValidation = new RegExp(/<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="(.*)" \/>/).exec(response.data)[1]
     Session.KeyID = new RegExp(/<input type="hidden" name="KeyID" id="KeyID" value="(.*)" \/>/).exec(response.data)[1]
     Session.TabLogin = Session.TabLogin.replace(/&quot;/g, '"')
+    Session.Username = username
 }
 
 let Password = async password => {
+    Session.SecureCookieKey = "Secure_" + Session.Username
     let response = await axios.post('https://www.w-w-i-s.com/hb/51/Default.aspx?entity=UYCI]', {
         "ScriptManagerContent_HiddenField": "",
         "tabLogin_ClientState": Session.TabLogin,
@@ -107,7 +126,7 @@ let Password = async password => {
     }, {
         headers: {
             "Host": "www.w-w-i-s.com",
-            "Cookie": "ASP.NET_SessionId="+Session.ID,
+            "Cookie": Session.SecureCookieKey+"=Test_Cookie50=Test to see if standard cookies are enabled.; ASP.NET_SessionId="+Session.ID,
             "Cache-Control": "max-age=0",
             "Sec-Ch-Ua": '"Chromium";v="107", "Not=A?Brand";v="24"',
             "Sec-Ch-Ua-Mobile": "?0",
@@ -126,15 +145,138 @@ let Password = async password => {
             "Accept-Language": "en-US,en;q=0.9"
         }
     })
-    console.log(response)
+    Session.EventTarget_ = new RegExp(/<input type="hidden" name="__EVENTTARGET" id="__EVENTTARGET" value="(.*)" \/>/).exec(response.data)[1]
+    Session.EventArgument_ = new RegExp(/<input type="hidden" name="__EVENTARGUMENT" id="__EVENTARGUMENT" value="(.*)" \/>/).exec(response.data)[1]
+    Session.ViewState = new RegExp(/<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="(.*)" \/>/).exec(response.data)[1]
+    Session.ViewStateGenerator = new RegExp(/<input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="(.*)" \/>/).exec(response.data)[1]
+    Session.EventValidation = new RegExp(/<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="(.*)" \/>/).exec(response.data)[1]
+    Session.TabLogin = Session.TabLogin.replace(/&quot;/g, '"')
+    Session.Question = new RegExp(/<label for="TFAQuestionPrompt1_Answer" id="TFAQuestionPrompt1_Question">(.*)<\/label>/).exec(response.data)[1]
+    Session.KeyID = new RegExp(/<input type="hidden" name="KeyID" value="(.*)" \/>/).exec(response.data)[1]
 }
 
+let Question = async () => {
+    let response = await axios.post('https://www.w-w-i-s.com/hb/51/login.aspx?KeyID='+Session.KeyID+'&flash=no', {
+        "__EVENTTARGET": "",
+        "__EVENTARGUMENT": "",
+        "__VIEWSTATE": Session.ViewState,
+        "__VIEWSTATEGENERATOR": Session.ViewStateGenerator,
+        "__EVENTVALIDATION": Session.EventValidation,
+        "KeyID": Session.KeyID,
+        "TFAQuestionPrompt1$Answer": Session.Answer,
+        "TFAQuestionPrompt1$cmdTFAQuestionsCont": "Continue"
+    }, {
+        headers: {
+            "Host": "www.w-w-i-s.com",
+            "Cookie": Session.SecureCookieKey+"=Test_Cookie50=Test to see if standard cookies are enabled.; ASP.NET_SessionId="+Session.ID,
+            "Cache-Control": "max-age=0",
+            "Sec-Ch-Ua": '"Chromium";v="107", "Not=A?Brand";v="24"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": "Windows",
+            "Upgrade-Insecure-Requests": "1",
+            "Origin": "https://www.w-w-i-s.com",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.107 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-User": "?1",
+            "Sec-Fetch-Dest": "document",
+            "Referer": 'https://www.w-w-i-s.com/hb/51/login.aspx?KeyID='+Session.KeyID+'&flash=no',
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en-US,en;q=0.9"
+        }
+    })
+    // save session data
+    Session.EventTarget_ = new RegExp(/<input type="hidden" name="__EVENTTARGET" id="__EVENTTARGET" value="(.*)" \/>/).exec(response.data)[1]
+    Session.EventArgument_ = new RegExp(/<input type="hidden" name="__EVENTARGUMENT" id="__EVENTARGUMENT" value="(.*)" \/>/).exec(response.data)[1]
+    Session.ViewState = new RegExp(/<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="(.*)" \/>/).exec(response.data)[1]
+    Session.ViewStateGenerator = new RegExp(/<input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="(.*)" \/>/).exec(response.data)[1]
+    Session.EventValidation = new RegExp(/<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="(.*)" \/>/).exec(response.data)[1]
+    Session.TabLogin = Session.TabLogin.replace(/&quot;/g, '"')
+    Session.KeyID = new RegExp(/<input type="hidden" name="KeyID" value="(.*)" \/>/).exec(response.data)[1]
+    let accept = await axios.post('https://www.w-w-i-s.com/hb/51/login.aspx?KeyID='+Session.KeyID+'&flash=no', {
+        "__EVENTTARGET": Session.EventTarget_,
+        "__EVENTARGUMENT": Session.EventArgument_,
+        "__VIEWSTATE": Session.ViewState,
+        "__VIEWSTATEGENERATOR": Session.ViewStateGenerator,
+        "__EVENTVALIDATION": Session.EventValidation,
+        "KeyID": Session.KeyID,
+        "cmdNoPromptContinue": "Continue"
+    }, {
+        headers: {
+            "Host": "www.w-w-i-s.com",
+            "Cookie": Session.SecureCookieKey+"=Test_Cookie50=Test to see if standard cookies are enabled.; ASP.NET_SessionId="+Session.ID,
+            "Cache-Control": "max-age=0",
+            "Sec-Ch-Ua": '"Chromium";v="107", "Not=A?Brand";v="24"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": "Windows",
+            "Upgrade-Insecure-Requests": "1",
+            "Origin": "https://www.w-w-i-s.com",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.107 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-User": "?1",
+            "Sec-Fetch-Dest": "document",
+            "Referer": 'https://www.w-w-i-s.com/hb/51/login.aspx?KeyID='+Session.KeyID+'&flash=no',
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en-US,en;q=0.9"
+        }
+    })
+    Session.EventTarget_ = new RegExp(/<input type="hidden" name="__EVENTTARGET" id="__EVENTTARGET" value="(.*)" \/>/).exec(accept.data)[1]
+    Session.EventArgument_ = new RegExp(/<input type="hidden" name="__EVENTARGUMENT" id="__EVENTARGUMENT" value="(.*)" \/>/).exec(accept.data)[1]
+    Session.ViewState = new RegExp(/<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="(.*)" \/>/).exec(accept.data)[1]
+    Session.EventValidation = new RegExp(/<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="(.*)" \/>/).exec(accept.data)[1]
+    Session.TabLogin = Session.TabLogin.replace(/&quot;/g, '"')
+    let page = accept.data
+    let matches = page.match(/<td><a id="Landing1_AccountBox1_AccountRepeater_ctl[0-9][0-9]_History" href="(.*)">(.*)<\/a><\/td>/gi)
+    for (let match of matches) {
+        let id = new RegExp(/ctl[0-9][0-9]/).exec(match)[0]
+        let account = new RegExp(/href="(.*)">(.*)<\/a><\/td>/).exec(match)[2]
+        let penbal = new RegExp('<td id="Landing1_AccountBox1_AccountRepeater_'+id+'_BalCol1TCell" class="Money">(.*)<\/td>').exec(page)[1]
+        let bal = new RegExp('<td id="Landing1_AccountBox1_AccountRepeater_'+id+'_BalCol1TCell" class="Money">.*<\\/td>[\\n\\s]+<td class="Money">(\\$[0-9.]+)').exec(page)[1]
+        Data.Accounts.push({
+            id: id,
+            account: account,
+            penbal: penbal,
+            bal: bal
+        })
+    }
+
+}
+
+let prompt = require('prompt-sync')();
 
 let Login = async (username, password) => {
+    console.log("Creating Session...")
     await CreateSession()
+    console.log("Setting Username...")
     await Username(username)
+    console.log("Setting Password...")
     await Password(password)
+    console.log("Logging in...")
+    let questions
+    if (fs.existsSync('questions.json')) {
+        questions = JSON.parse(fs.readFileSync('questions.json'))
+    } else {
+        questions = {}
+    }
+    if (questions[username] == undefined || questions[username][Session.Question] == undefined) {
+        console.log(Session.Question)
+        Session.Answer = prompt('Answer: ')
+        if (questions[username] == undefined) questions[username] = {}
+        questions[username][Session.Question] = Session.Answer
+    } else {
+        Session.Answer = questions[username][Session.Question]
+    }
+    console.log("Answering Question...")
+    await Question()
     console.log(Session)
+    console.log(Data)
+    fs.writeFile('questions.json', JSON.stringify(questions), () => {})
 }
 
-Login('username', 'password')
+require('dotenv').config()
+Login(process.env.USERNAME, process.env.PASSWORD)
