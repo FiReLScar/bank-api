8543537fe3c1c4aec85a119adfd1b4d93bdc7605
FiRe <levih@n11.dev>
2022-12-15 04:10:47 -0600

    Transactions: finished transactions
    Signed-off-by: FiRe <levih@n11.dev>

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ let Question = async () => {
         let id = new RegExp(/ctl[0-9][0-9]/).exec(match)[0]
         let account = new RegExp(/href="(.*)">(.*)<\/a><\/td>/).exec(match)[2]
         let penbal = new RegExp('<td id="Landing1_AccountBox1_AccountRepeater_'+id+'_BalCol1TCell" class="Money">(.*)<\/td>').exec(page)[1]
-        let bal = new RegExp('<td id="Landing1_AccountBox1_AccountRepeater_'+id+'_BalCol1TCell" class="Money">.*<\\/td>[\\n\\s]+<td class="Money">(\\$[0-9.]+)').exec(page)[1]
+        let bal = new RegExp('<td id="Landing1_AccountBox1_AccountRepeater_'+id+'_BalCol1TCell" class="Money">.*<\\/td>[\\n\\s]+<td class="Money">((-|)\\$[0-9.]+)').exec(page)[1]
         Data.Accounts.push({
             id: id,
             account: account,
@@ -244,7 +244,132 @@ let Question = async () => {
             bal: bal
         })
     }
+}
 
+let UpdateBalance = async () => {
+    let response = await axios.post('https://www.w-w-i-s.com/hb/51/Account.aspx?KeyID='+Session.KeyID, {
+        "ScriptManagerContent": "ScriptManagerContent|lkViewAccounts",
+        "ScriptManagerContent_HiddenField": "",
+        "KeyID": Session.KeyID,
+        "__EVENTTARGET": "lkViewAccounts",
+        "__EVENTARGUMENT": "",
+        "__LASTFOCUS": "",
+        "__VIEWSTATEKEY": "ViewState_3",
+        "__VIEWSTATE": "",
+        "__EVENTVALIDATION": Session.EventValidation,
+        "__ASYNCPOST": "true",
+    }, {
+        headers: {
+            "Host": "www.w-w-i-s.com",
+            "Cookie": Session.SecureCookieKey+"=Test_Cookie50=Test to see if standard cookies are enabled.; ASP.NET_SessionId="+Session.ID,
+            "Cache-Control": "max-age=0",
+            "Sec-Ch-Ua": '"Chromium";v="107", "Not=A?Brand";v="24"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": "Windows",
+            "Upgrade-Insecure-Requests": "1",
+            "Origin": "https://www.w-w-i-s.com",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.107 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-User": "?1",
+            "Sec-Fetch-Dest": "document",
+            "Referer": 'https://www.w-w-i-s.com/hb/51/login.aspx?KeyID='+Session.KeyID+'&flash=no',
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en-US,en;q=0.9"
+        }
+    })
+    let page = response.data
+    let matches = page.match(/<td><a id="Landing1_AccountBox1_AccountRepeater_ctl[0-9][0-9]_History" href="(.*)">(.*)<\/a><\/td>/gi)
+    Data.Accounts = []
+    for (let match of matches) {
+        let id = new RegExp(/ctl[0-9][0-9]/).exec(match)[0]
+        let account = new RegExp(/href="(.*)">(.*)<\/a><\/td>/).exec(match)[2]
+        let penbal = new RegExp('<td id="Landing1_AccountBox1_AccountRepeater_'+id+'_BalCol1TCell" class="Money">(.*)<\/td>').exec(page)[1]
+        let bal = new RegExp('<td id="Landing1_AccountBox1_AccountRepeater_'+id+'_BalCol1TCell" class="Money">.*<\\/td>[\\n\\s]+<td class="Money">((-|)\\$[0-9.]+)').exec(page)[1]
+        Data.Accounts.push({
+            id: id,
+            account: account,
+            penbal: penbal,
+            bal: bal
+        })
+    }
+}
+
+let GetHistory = async (id) => {
+    let response = await axios.post('https://www.w-w-i-s.com/hb/51/Account.aspx?KeyID='+Session.KeyID, {
+        "ScriptManagerContent": "UpdatePanelContentSection|Landing1$AccountBox1$AccountRepeater$"+id+"$History",
+        "ScriptManagerContent_HiddenField": "",
+        "__EVENTTARGET": "Landing1$AccountBox1$AccountRepeater$"+id+"$History",
+        "__EVENTARGUMENT": "",
+        "__VIEWSTATEKEY": "ViewState_1",
+        "__VIEWSTATE": "",
+        "__EVENTVALIDATION": Session.EventValidation,
+        "KeyID": Session.KeyID,
+        "__ASYNCPOST": "true",
+    }, {
+        headers: {
+            "Host": "www.w-w-i-s.com",
+            "Cookie": Session.SecureCookieKey+"=Test_Cookie50=Test to see if standard cookies are enabled.; ASP.NET_SessionId="+Session.ID,
+            "Cache-Control": "max-age=0",
+            "Sec-Ch-Ua": '"Chromium";v="107", "Not=A?Brand";v="24"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": "Windows",
+            "Upgrade-Insecure-Requests": parseInt(id.substring(3)),
+            "Origin": "https://www.w-w-i-s.com",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.107 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-User": "?1",
+            "Sec-Fetch-Dest": "document",
+            "Referer": 'https://www.w-w-i-s.com/hb/51/Account.aspx?KeyID='+Session.KeyID,
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en-US,en;q=0.9"
+        }
+    })
+    let page = response.data
+    let PendTransDiv = page.indexOf('<div id="HistoryBox1_PendTransactionsDiv">')+'<div id="HistoryBox1_PendTransactionsDiv">'.length
+    let PendTransEnd = page.indexOf('</div>', PendTransDiv)
+    let PendTable = page.substring(PendTransDiv, PendTransEnd)
+    let matches = PendTable.match(/<td><a id="HistoryBox1_PendTransactionsGrid_ctl[0-9][0-9]_PendDateLink" href=".*">([0-9]+\/[0-9]+\/[0-9]*)<br>.*<\/a><\/td><td>(.*)<\/td><td class="Money">(&nbsp;|\$[0-9.]+)<\/td><td class="Money">(&nbsp;|\$[0-9.]+)<\/td>/gi)
+    let transactions = []
+    if (matches!=null) {
+        for (let match of matches) {
+            let data = new RegExp(/<td><a id="HistoryBox1_PendTransactionsGrid_ctl[0-9][0-9]_PendDateLink" href=".*">([0-9]+\/[0-9]+\/[0-9]*)<br>.*<\/a><\/td><td>(.*)<\/td><td class="Money">(&nbsp;|(-|)\$[0-9.]+)<\/td><td class="Money">(&nbsp;|(-|)\$[0-9.]+)<\/td>/gi).exec(match)
+            let date = data[1]
+            let desc = data[2]
+            let money = data[3]=="&nbsp;"?data[5]:data[3]
+            console.log(data[1], data[2], data[3], data[4], data[5], data[6], data[7])
+            transactions.push({
+                date: date,
+                desc: desc,
+                money: money,
+                balance: null,
+                pending: true
+            })
+        }
+    }
+    matches = page.match(/<td><a id="HistoryBox1_LastTransactionGrid_ctl[0-9]+_HistDateLink" href=".*">([0-9]+\/[0-9]+\/[0-9]+)<\/a>\s*<\/td><td>(.*)<\/td><td class="Money">(-\$[0-9.]+|)<\/td><td class="Money">(\$[0-9.]+|)<\/td><td class="Money">((-|)\$[0-9.]+|)<\/td>/gi)
+    if (matches!=null) {
+        for (let match of matches) {
+            let data = new RegExp(/<td><a id="HistoryBox1_LastTransactionGrid_ctl[0-9]+_HistDateLink" href=".*">([0-9]+\/[0-9]+\/[0-9]+)<\/a>\s*<\/td><td>(.*)<\/td><td class="Money">(-\$[0-9.]+|)<\/td><td class="Money">(\$[0-9.]+|)<\/td><td class="Money">((-|)\$[0-9.]+|)<\/td>/gi).exec(match)
+            let date = data[1]
+            let desc = data[2]
+            let money = data[3]==""?data[4]:data[3]
+            let balance = data[5]
+            transactions.push({
+                date: date,
+                desc: desc,
+                money: money,
+                balance: balance,
+                pending: false
+            })
+        }
+    }
+    console.log(transactions)
 }
 
 let prompt = require('prompt-sync')();
@@ -274,8 +399,12 @@ let Login = async (username, password) => {
     console.log("Answering Question...")
     await Question()
     console.log(Session)
-    console.log(Data)
     fs.writeFile('questions.json', JSON.stringify(questions), () => {})
+    console.log("Updating Balance...")
+    await UpdateBalance()
+    console.log(Data)
+    console.log("Getting History...")
+    await GetHistory(Data.Accounts[1].id)
 }
 
 require('dotenv').config()
